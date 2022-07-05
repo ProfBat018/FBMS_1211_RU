578d3da4a27a12878f742fb979c213996770e87a
Elvin Azimov <e.azimov@itstep.edu.az>
2022-07-05 11:04:52 +0400

    Thread Pool lesson

---
+++ C#10/SealedMethod/SealedMethod/Program.cs
@@ -0,0 +1,27 @@
+﻿class Person
+{
+    public Person(string name, string surname, int age)
+    {
+        Name = name;
+        Surname = surname;
+        Age = age;
+    }
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public int Age { get; set; }
+
+    public sealed override string ToString()
+    {
+        return $"{Name}\n{Surname}\n{Age}\n";
+    }
+}
+
+class Student : Person
+{
+    public Student(string name, string surname, int age) : base(name, surname, age)
+    {
+
+    }
+
+}
+
---
+++ SystemProgramming/ThreadPool/ThreadPool/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadPoolLesson
+{
+    internal class Program
+    {
+        static void Foo(object index)
+        {
+            Console.WriteLine($"Foo Started: {Thread.CurrentThread.IsThreadPoolThread}\t{Thread.CurrentThread.ManagedThreadId}\t {Thread.CurrentThread.IsBackground}");
+        }
+
+
+        static void ThreadExample()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Thread thread = new Thread(Foo);
+                thread.Start(i);
+            }
+        }
+
+        static void ThreadPoolExample()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                ThreadPool.QueueUserWorkItem(new WaitCallback(param => Foo(i)));
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Process.GetCurrentProcess().Threads.Count);
+
+            ProcessThreadCollection thread_count = Process.GetCurrentProcess().Threads;
+
+            foreach (ProcessThread item in thread_count)
+            {
+                Console.WriteLine(item.ThreadState);
+            }
+
+            Stopwatch stopwatch = new Stopwatch();
+
+            stopwatch.Start();
+            ThreadExample();
+            stopwatch.Stop();
+
+            var a = stopwatch.Elapsed;
+            stopwatch.Reset();
+
+
+            stopwatch.Start();
+            ThreadPoolExample();
+            stopwatch.Stop();
+            var b = stopwatch.Elapsed;
+            stopwatch.Reset();
+
+            Thread.Sleep(1000);
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(Process.GetCurrentProcess().Threads.Count);
+
+            thread_count = Process.GetCurrentProcess().Threads;
+            foreach (ProcessThread item in thread_count)
+            {
+                Console.WriteLine(item.ThreadState);
+            }
+        }
+    }
+}
