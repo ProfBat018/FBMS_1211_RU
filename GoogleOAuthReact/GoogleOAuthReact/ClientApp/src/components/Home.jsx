a20a66b54ef61202a8ad9cf9455bd6345a904dd2
Elvin Azimov <elvin.azim01@gmail.com>
2023-04-06 21:08:34 +0400

    dotnetaz

---
+++ GoogleOAuthReact/GoogleOAuthReact/ClientApp/src/components/Home.jsx
@@ -0,0 +1,13 @@
+﻿import React from 'react';
+import {Layout} from "./Layout";
+import Auth from "./Auth";
+
+const Home = () => {
+    return (
+        <div>
+            <Auth/>
+        </div>
+    );
+};
+
+export default Home;
---
+++ GoogleOAuthReact/GoogleOAuthReact/Controllers/AuthController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GoogleOAuthReact.Controllers;
+
+public class AuthController : Controller
+{
+
+    public SignInManager<User> signInManager = new();
+    // GET
+    [HttpPost]
+    [AllowAnonymous]
+    [Route("account/external-login")]
+    public IActionResult ExternalLogin(string provider, string returnUrl)
+    {
+        var redirectUrl = $"https://api.domain.com/identity/v1/account/external-auth-callback?returnUrl={returnUrl}";
+        var properties = signInManager.ConfigureExternalAuthenticationProperties(provider, redirectUrl);
+        properties.AllowRefresh = true;
+        return Challenge(properties, provider);
+    }
+}
+
+public class User
+{
+}
