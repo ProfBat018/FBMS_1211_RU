d6d07c1f0dd91404d742d42131433c11e858f545
Elvin Azimov <elvin.azim01@gmail.com>
2023-04-30 18:47:32 +0400

    Blob lesson

---
+++ BlobSAS/BlobSAS/Program.cs
@@ -0,0 +1,89 @@
+﻿#region GET
+/*
+using System.Net;
+using System.Text;
+
+UploadFileToAzureBlobStorage("Tamerlan dvoecnik", "tamerlan.txt");
+
+void UploadFileToAzureBlobStorage(string content, string fileName)
+{
+    string sasToken = "sp=racwl&st=2023-04-27T17:24:12Z&se=2023-04-28T01:24:12Z&sv=2021-12-02&sr=c&sig=bItH9eQEjcEcO2xYGDkFTLBADOKyVW%2FXn6Y0ZpG3MGY%3D";
+    string storageAccount = "nightcall";
+    string containerName = "images";
+    string blobName = fileName;
+
+    string method = "GET";
+    string sampleContent = content;
+    int contentLength = Encoding.UTF8.GetByteCount(sampleContent);
+
+    string requestUri = $"https://{storageAccount}.blob.core.windows.net/{containerName}/{blobName}?{sasToken}";
+
+    HttpWebRequest request = (HttpWebRequest)WebRequest.Create(requestUri);
+    request.Method = method;
+    request.ContentType = "text/plain; charset=UTF-8";
+    request.ContentLength = contentLength;
+    request.Headers.Add("x-ms-blob-type", "BlockBlob");
+
+    using Stream requestStream = request.GetRequestStream();
+    using FileStream fs = new("tamerlan.txt", FileMode.CreateNew);
+    
+    byte[] buffer = new byte[1024];
+    
+    requestStream.Read(buffer, 0, buffer.Length);
+    fs.Write(buffer, 0, buffer.Length);
+
+    using (HttpWebResponse resp = (HttpWebResponse)request.GetResponse())
+    {
+        if (resp.StatusCode == HttpStatusCode.OK)
+        {
+            Console.WriteLine("Success");
+        }
+        else
+        {
+            Console.WriteLine($"Fail: {resp.StatusCode}");
+        }
+    }
+}
+*/
+#endregion
+
+#region PUT
+/*
+using System.Net;
+using System.Text;
+using System.IO;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.ComponentModel;
+using System.Reflection.Metadata;
+using System.Xml.Linq;
+using System;
+
+
+await DownloadFileFromAzureBlobStorage("Yoda-HTML.png");
+
+async Task DownloadFileFromAzureBlobStorage(string fileName)
+{
+    string connectionString = "DefaultEndpointsProtocol=https;AccountName=nightcall;AccountKey=vE3Z13s8NKpxFftJOgGweG5VM+CrR3qYM0ho80Tpg8gYaMSC3n156klcS2wMWeZsXJPY9ETFv/r1+ASt9H7jkw==;EndpointSuffix=core.windows.netDefaultEndpointsProtocol=https;AccountName=nightcall;AccountKey=vE3Z13s8NKpxFftJOgGweG5VM+CrR3qYM0ho80Tpg8gYaMSC3n156klcS2wMWeZsXJPY9ETFv/r1+ASt9H7jkw==;EndpointSuffix=core.windows.net";
+    string storageAccount = "nightcall";
+    string containerName = "images";
+    string blobName = fileName;
+
+    string requestUri = $"https://{storageAccount}.blob.core.windows.net/{containerName}/{blobName}?{connectionString}";
+
+    using var httpClient = new HttpClient();
+
+    using var stream = await httpClient.GetStreamAsync(requestUri);
+
+    using var downloadedFileStream = new FileStream(fileName, FileMode.Create);
+
+    await stream.CopyToAsync(downloadedFileStream);
+
+    Console.WriteLine("File downloaded successfully.");
+}
+
+*/
+#endregion
+
+
+
---
+++ Lessons/Lesson6.md
@@ -0,0 +1,32 @@
+# Azure Compute Services 
+
+## Введение в Azure Compute Services
+* Azure Compute Services - это сервисы, которые позволяют создавать и управлять виртуальными машинами и контейнерами в Azure.
+* Azure Compute Services включает в себя следующие сервисы:
+  * Virtual Machines
+  * Virtual Machine Scale Sets
+  * Azure Container Instances
+  * Azure Kubernetes Service
+  * Azure Batch
+
+## Virtual Machines
+
+* Azure Virtual Machines - это сервис, который позволяет развернуть виртуальные машины в Azure.
+
+## Virtual Machine Scale Sets
+* Azure Virtual Machine Scale Sets - это сервис, который позволяет развернуть и управлять группой однотипных виртуальных машин в Azure.
+
+
+## Azure Container Instances
+* Azure Container Instances - это сервис, который позволяет развернуть контейнеры в Azure без необходимости управлять виртуальными машинами и контейнерными оркестраторами.
+
+Что такое контейнеры?
+* Контейнеры - это способ упаковки приложения и всех его зависимостей в единый файл, который может быть запущен на любой операционной системе.
+
+## Azure Kubernetes Service
+* Azure Kubernetes Service - это сервис, который позволяет развернуть и управлять кластером Kubernetes в Azure.
+
+## Azure Batch
+* Azure Batch - это сервис, который позволяет развернуть и управлять пакетными вычислениями в Azure.
+
+
