7e971100d36458e43db36c28a721d06b71a5f051
Elvin Azimov <elvin.azim01@gmail.com>
2023-05-23 19:17:53 +0400

    DevOps created

--- ASP/AzimEx(JWT)/AzimEx/Services/Configurations/IConfigService.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AzimEx.Services.Configurations
-{
-    public interface IConfigService
-    {
-        public string? IConfigPath { get; set; }
-        public IConfigurationRoot BuildConfiguration();
-        public string? GetValue(string? key);
-    }
-}
