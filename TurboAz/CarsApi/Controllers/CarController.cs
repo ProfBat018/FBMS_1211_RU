d46e08487bad6d31f2a228b757350f5b297dec7b
Elvin Azimov <elvin.azim01@gmail.com>
2023-06-01 08:47:15 +0400

    Updates

---
+++ TurboAz/CarsApi/Controllers/CarController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarsApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CarController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Index()
+    {
+        using FileStream fs = new("carsData.json", FileMode.OpenOrCreate);
+        using StreamReader sr = new(fs);
+        string json = sr.ReadToEnd();
+
+        return Ok(json);
+    }
+}
