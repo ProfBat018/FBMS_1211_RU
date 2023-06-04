79c8c00047a924dfd1b3779024a3556d8115c60e
Elvin Azimov <elvin.azim01@gmail.com>
2023-06-04 22:45:10 +0400

    Updated

--- TurboAz/CarsApi/Controllers/CarController.cs
+++ TurboAz/CarsApi/Controllers/CarController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
 
 namespace CarsApi.Controllers;
 
@@ -13,6 +14,38 @@ public class CarController : ControllerBase
         using StreamReader sr = new(fs);
         string json = sr.ReadToEnd();
 
+        return Ok(json);
+    }
+
+    [HttpPost]
+    public IActionResult Addcar(string make, string model, string url)
+    {
+        Car car = new()
+        {
+            Make = make,
+            Model = model,
+            Url = url
+        };
+
+        using FileStream fs = new("carsData.json", FileMode.OpenOrCreate);
+        using StreamReader sr = new(fs);
+
+        string json = sr.ReadToEnd();
+
+        CarsData cars = JsonSerializer.Deserialize<CarsData>(json);
+
+        cars.Cars.Add(car);
+
+        fs.Close();
+
+        using FileStream fs2 = new ("carsData.json", FileMode.Truncate);
+        
+        string newJson = JsonSerializer.Serialize(cars);
+
+        using StreamWriter sw = new(fs);
+
+        sw.Write(newJson);
+        
         return Ok(json);
     }
 }
